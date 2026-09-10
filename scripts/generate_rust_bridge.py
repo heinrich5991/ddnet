804b64465d0c0639f25bb50ab60b19a2c85dc764
heinrich5991 <heinrich5991@gmail.com>
2026-09-10 14:20:14 +0200

    i

--- scripts/generate_rust_bridge.py
+++ scripts/generate_rust_bridge.py
@@ -34,6 +34,7 @@ def find_cxxbridge(version):
 
 FILES = {
 	"src/engine/shared/rust_version.rs": "src/rust-bridge/engine/shared/rust_version",
+	"src/engine/server/lib.rs": "src/rust-bridge/server/lib",
 	"src/engine/console.rs": "src/rust-bridge/cpp/console",
 }
 
